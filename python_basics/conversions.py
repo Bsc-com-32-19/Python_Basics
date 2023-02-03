87ce48aa46192cf56a8d75fdbf3984a22336ade1
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-03 11:09:59 +0200

    added conversion and string classes

---
+++ python_basics/conversions.py
@@ -0,0 +1,9 @@
+# anytime you use input function you always geta string
+birth_year = input('Birth year: ')
+age = 2023 - int(birth_year)
+print(age)
+
+# Exercise
+weight = input('Enter your weight in pounds: ')
+weight_kg = int(weight) * 0.45
+print(weight_kg)
---
+++ python_basics/strings.py
@@ -0,0 +1,29 @@
+course = "Python's course for beginners"
+print(course)
+course = 'Python course for "beginners"'
+print(course)
+
+# string with muitiple lines
+letter = '''
+Hie Mimie,
+
+Here is our first gift to you.
+ 
+ Thank you,
+ Your's lovely mumie.
+ 
+'''
+print(letter)
+
+# printing the characters according to index // python is zero index
+course = 'python for beginners'
+print(course[0:4])
+
+# here variable another will take the value of a course variable
+course = 'python for beginners'
+another = course[:]
+print(another)
+
+# Exercise
+name = 'Olivia'
+print(name[0:])
