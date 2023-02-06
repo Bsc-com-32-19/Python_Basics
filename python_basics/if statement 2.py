417be648a9b8ad7332740afc3bf854ea525c3cab
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-06 10:46:25 +0200

    added if_statement classes

---
+++ python_basics/if statement 2.py
@@ -0,0 +1,16 @@
+# define a boolean value
+is_hot =False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day")
+    print("Dink plenty of water")
+
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
+
---
+++ python_basics/if statement.py
@@ -0,0 +1,18 @@
+# define a boolean value
+is_hot = False
+
+if is_hot:
+    print("It's a hot day")
+    print("Dink plenty of water")
+
+else:
+    print("It's a cold day")
+    print("Wear warm clothes")
+print("Enjoy your day")
+
+# The second prints if the statement is false for the first print statement
+# is_hot = False
+
+# if is_hot:
+#  print("It's a hot day")
+# print("Enjoy your day")
