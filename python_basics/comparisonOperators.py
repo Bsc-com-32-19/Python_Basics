62af2e1ff312aa7344d53752ce169d1ee0fa7eb1
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-08 13:40:24 +0200

    added comparison_operator & weight_convertor class

---
+++ python_basics/comparisonOperators.py
@@ -0,0 +1,19 @@
+# you print values depending on the number
+temperature = 30
+
+if temperature > 30:
+    print("It is a hot day ")
+else:
+    print("It is not a hot day")
+
+# Exercise
+name ="mimie"
+
+if len(name) <= 3 :
+    print("name must be at least 3 characters")
+
+elif len(name) > 50:
+    print("name can be maximum of 50")
+
+else:
+    print("name looks good")
