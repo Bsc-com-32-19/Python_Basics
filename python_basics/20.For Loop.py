3fad7caab26c5b5d879ad44cdda36c570570da0a
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-09 23:08:09 +0200

    added a for_loop class

---
+++ python_basics/20.For Loop.py
@@ -0,0 +1,22 @@
+for item in 'python':
+    print(item)
+
+# list of items
+for item in ['mimie', 'olivia','shira']:
+    print(item)
+
+# list of numbers
+for item in [1, 2, 3, 4, 5]:
+    print(item)
+
+# range of numbers using range function
+for item in range(10):
+     print(item)
+
+# start from 5 to 10
+for item in range(5,10):
+     print(item)
+
+# jump two number then print the 3rd number
+for item in range(5, 10, 2):
+     print(item)
