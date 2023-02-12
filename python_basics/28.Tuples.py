e7914c3a2eb205a33c56b6870704c0bd65e7ddf0
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-12 22:15:36 +0200

    added tuple class

---
+++ python_basics/28.Tuples.py
@@ -0,0 +1,6 @@
+# tuples are immutable lists they can not be changed
+numbers = (1, 2, 3)
+print(numbers[0])
+
+# index and count methods are allowed
+# and also the imaginable methods that start with under_scroll
