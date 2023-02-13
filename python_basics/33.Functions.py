4c349373baab4f62cf699211d3dc69e61f24d234
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-02-13 22:35:12 +0200

    added Functions

---
+++ python_basics/33.Functions.py
@@ -0,0 +1,9 @@
+# function is a container that contains lines of codes which are reusable
+def greet_user(): # defining the function
+    print('Hi there!')
+    print('Welcome dear')
+
+
+print("Start")
+greet_user()
+print("Finish")
